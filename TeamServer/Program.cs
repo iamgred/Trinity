64fc78d0196ea06bf847f78cf865bc9a94a0ecf4
gred <162592397+iamgred@users.noreply.github.com>
2026-07-20 01:06:23 -0700

    init

---
+++ TeamServer/Program.cs
@@ -0,0 +1,36 @@
+namespace TeamServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            if (app.Environment.IsDevelopment()) 
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = String.Empty;
+                });
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

d0f7bc46055b66accb6922b91096fda49ffe3a21
gred <162592397+iamgred@users.noreply.github.com>
2026-09-20 21:14:43 +0200

    feat: add tasks API controller endpoints

--- TeamServer/Controllers/Routes.cs
+++ TeamServer/Controllers/Routes.cs
@@ -6,6 +6,7 @@ namespace TeamServer.Controllers
         public const string Listeners = "/api/v1/listeners";
         public const string Server = "/api/v1/server";
         public const string Commands = "/api/v1/commands";
+        public const string Tasks = "/api/v1/tasks";
     }
 }
 //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ { END OF FILE } ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ //
---
+++ TeamServer/Controllers/TaskController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamServer.Controllers
+{
+    [Route(Routes.Tasks)]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public TaskController()
+        {
+            
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// Retrieves all tasks.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/tasks")]
+        public async Task<IActionResult> GetTasks() 
+        {
+            return Ok();
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// Retrieves a task by ID.
+        /// </summary>
+        /// <param name="taskID"></param>
+        /// <returns></returns>
+        [HttpGet("/tasks/{taskID")]
+        public async Task<IActionResult> GetTaskByID([FromRoute] int taskID)
+        {
+            return Ok();
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// Retrieves tasks associated to a specified agent.
+        /// </summary>
+        /// <param name="agentID"></param>
+        /// <returns></returns>
+        [HttpGet("/{agentID}")]
+        public async Task<IActionResult> GetTasksByAgentID([FromRoute] int agentID)
+        {
+            return Ok();
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// Retrieves all active downloads associated to a specified agent.
+        /// </summary>
+        /// <param name="agentID"></param>
+        /// <returns></returns>
+        [HttpGet("/{agentID}/activeDownloads")]
+        public async Task<IActionResult> GetActiveDownloadsByAgentID([FromRoute] int agentID)
+        {
+            return Ok();
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// De-queues a task with a pending state.
+        /// </summary>
+        /// <param name="taskID"></param>
+        /// <returns></returns>
+        [HttpGet("/{taskID}/stop")]
+        public async Task<IActionResult> StopTask([FromRoute] int taskID)
+        {
+            return Ok();
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+        /// <summary>
+        /// De-queues all tasks in the agent queue. 
+        /// </summary>
+        /// <param name="taskID"></param>
+        /// <returns></returns>
+        [HttpDelete("/{agentID}/clearQueue")]
+        public async Task<IActionResult> ClearQueue([FromRoute] int taskID) 
+        {
+            return Ok();
+        }
+    }
+}
